9b1f5b00571d843d7f9d5cdd9f38931e9908dd67
Lâm <105009339+iamlamm@users.noreply.github.com>
2025-03-13 11:12:03 +0700

    Update Constants.kt

--- app/src/main/java/com/healthtech/doccareplus/utils/Constants.kt
+++ app/src/main/java/com/healthtech/doccareplus/utils/Constants.kt
@@ -3,12 +3,12 @@ package com.healthtech.doccareplus.utils
 
 object Constants {
     // Zego App Chat
-    const val APP_ID: Long = 1651115921
-    const val APP_SIGN: String = "fb5b79b92c4f547b0e4bcd156780eed2df46dad1702f786c608ad335c9d0ffb8"
+    const val APP_ID: Long = 16511
+    const val APP_SIGN: String = ""
 
     // Cloudinary
-    const val CLOUDINARY_CLOUD_NAME = "daull03yv"
-    const val CLOUDINARY_API_KEY = "656698752728255"
-    const val CLOUDINARY_API_SECRET = "X84MgryT5lvKJdfSfjoQeOo7uXY"
-    const val CLOUDINARY_FOLDER_STORE_AVATAR = "doccareplus_avatars"
+    const val CLOUDINARY_CLOUD_NAME = ""
+    const val CLOUDINARY_API_KEY = ""
+    const val CLOUDINARY_API_SECRET = ""
+    const val CLOUDINARY_FOLDER_STORE_AVATAR = ""
 }
